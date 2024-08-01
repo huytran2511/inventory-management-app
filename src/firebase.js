3b0e68b7fd5e5f9ebba0c7c069b5313b67643754
huytran2511 <tranquanghuy999@gmail.com>
2024-07-31 18:05:35 -0700

    added firebase configs to vercel environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,22 @@ import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDsl-xo-TbLRm5g-pAakIufxneEA_m_6to",
+//   authDomain: "inventory-management-app-fe638.firebaseapp.com",
+//   projectId: "inventory-management-app-fe638",
+//   storageBucket: "inventory-management-app-fe638.appspot.com",
+//   messagingSenderId: "943703445828",
+//   appId: "1:943703445828:web:74faba31fc5152c285c711"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDsl-xo-TbLRm5g-pAakIufxneEA_m_6to",
-  authDomain: "inventory-management-app-fe638.firebaseapp.com",
-  projectId: "inventory-management-app-fe638",
-  storageBucket: "inventory-management-app-fe638.appspot.com",
-  messagingSenderId: "943703445828",
-  appId: "1:943703445828:web:74faba31fc5152c285c711"
+  apiKey: "process.env.FIREBASE_API_KEY",
+  authDomain: "process.env.FIREBASE_AUTH_DOMAIN",
+  projectId: "process.env.FIREBASE_PROJECT_ID",
+  storageBucket: "process.env.FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "process.env.FIREBASE_MESSAGING_SENDER_ID",
+  appId: "process.env.FIREBASE_APP_ID"
 };
 
 // Initialize Firebase
